29851fcb0597b31f464263228dc64d8c73d5a8f1
Unai Dorado <udorburu@gmail.com>
2025-08-13 01:33:20 +0200

    Corrected mouse hover on hover-btn not getting to default size when passing over icons

--- assets/scripts/cursor.js
+++ assets/scripts/cursor.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const span = document.querySelectorAll('span');
+    const span = document.querySelectorAll('.hover-btn');
     const cursor = document.querySelector('.cursor');
     const text = document.querySelectorAll('.text');
+        const body = document.querySelector('body');
     const defaultCursorSize = 75;
     const smallCursorSize = 50;
     const largeCursorSize = 150;
@@ -19,31 +20,30 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     });
 
-    window.addEventListener('mouseout', () =>{
-        gsap.to(cursor, {
-            '--dot-size': `${0}px`,
-            duration: 0.3,
-            ease: 'power2.out'
-        })
-    })
-
-    window.addEventListener('mouseover', () =>{
+    body.addEventListener('mouseenter', () => {
         gsap.to(cursor, {
             '--dot-size': `${defaultCursorSize}px`,
-
-            duration: 0.3,
-            ease: 'power2.out'
-        })
-    })
-
-    span.forEach(element => {
-        element.addEventListener('mouseenter', () => {
-        gsap.to(cursor, {
-            '--dot-size': `${smallCursorSize}px`, 
             duration: 0.3,
             ease: 'power2.out'
         });
     });
+    
+    body.addEventListener('mouseleave', () => {
+        gsap.to(cursor, {
+            '--dot-size': `${0}px`,
+            duration: 0.3,
+            ease: 'power2.out'
+        });
+    });
+
+    span.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            gsap.to(cursor, {
+                '--dot-size': `${smallCursorSize}px`,
+                duration: 0.3,
+                ease: 'power2.out'
+            });
+        });
 
         element.addEventListener('mouseleave', () => {
             gsap.to(cursor, {
