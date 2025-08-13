f0945ccd44b9c5caabb0f6dd458b146e50b4b1b6
Unai Dorado <udorburu@gmail.com>
2025-08-13 15:11:30 +0200

    Improved custom cursor system and generalized size change animations

--- assets/scripts/cursor.js
+++ assets/scripts/cursor.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const span = document.querySelectorAll('.hover-btn');
     const cursor = document.querySelector('.cursor');
-    const text = document.querySelectorAll('.text');
-        const body = document.querySelector('body');
+    const hoverableItem = document.querySelectorAll('.hoverableItem');
+    const text = document.querySelectorAll('.hoverableText');
+    const html = document.querySelector('html');
     const defaultCursorSize = 75;
     const smallCursorSize = 50;
     const largeCursorSize = 150;
     lucide.createIcons();
 
     window.addEventListener('mousemove', (e) => {
-        updateCursorPosition(e);
-        
+        const x = e.clientX;
+        const y = e.clientY;
+
+        gsap.to(cursor, {
+            x: x,
+            y: y,
+            duration: 2,
+            ease: 'elastic.out'
+        });
     });
 
-    window.addEventListener('scroll', (e) => {
-        updateCursorPosition(e);
-    });
-
-    body.addEventListener('mouseenter', () => {
+    html.addEventListener('mouseenter', () => {
         gsap.to(cursor, {
             '--dot-size': `${defaultCursorSize}px`,
             duration: 0.3,
@@ -25,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
     
-    body.addEventListener('mouseleave', () => {
+    html.addEventListener('mouseleave', () => {
         gsap.to(cursor, {
             '--dot-size': `${0}px`,
             duration: 0.3,
@@ -33,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    span.forEach(element => {
+    hoverableItem.forEach(element => {
         element.addEventListener('mouseenter', () => {
             gsap.to(cursor, {
                 '--dot-size': `${smallCursorSize}px`,
@@ -68,16 +71,4 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     })
-
-    function updateCursorPosition(e) {
-        const x = Math.round(e.clientX / window.innerWidth * 100);
-        const y = Math.round((e.clientY + window.scrollY) / window.innerHeight * 100);
-
-        gsap.to(cursor, {
-            '--x': `${x}%`,
-            '--y': `${y}%`,
-            duration: 2,
-            ease: 'elastic.out'
-        })
-    }
 });
